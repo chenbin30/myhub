aa478ddbadfaed27f38696cae9f7988aeaa001d8
chenbin <bingo_chen@163.com>
2017-10-23 23:27:56 +0800

    提交工程啦

---
+++ springboot-01/src/main/java/cn/itcast/springboot/controller/HelloController.java
@@ -0,0 +1,35 @@
+package cn.itcast.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+	
+	@Autowired
+	private Environment environment;
+	
+	@Value("${name}")
+	private String name;
+	@Value("${url}")
+	private String url;
+
+	
+	@GetMapping("/hello")
+	public String hello(){
+		
+		//System.out.println(environment.getProperty("name"));
+		System.out.println(environment.getProperty("url"));
+		
+		return "hello world";
+	}
+	
+	@GetMapping("/user")
+	public String user(){
+		return "/html/user.html";
+	}
+	
+}
